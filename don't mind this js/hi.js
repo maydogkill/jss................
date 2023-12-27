fc3224efa7ee4801ee85b568ba74c3a411adccda
Nuosect <135092132+maydogkill@users.noreply.github.com>
2023-12-27 18:51:39 +0700

    Add files via upload

---
+++ don't mind this js/hi.js
@@ -0,0 +1,19 @@
+// print("hello world")
+console.log('Hello World')
+
+let skibidi = 123123.321321
+const asdasd = 987
+var dsadsa = "skibidi"
+
+// console.log(5+5-5+5-5)
+
+// let arrey = []
+// arrey.push(1)
+// arrey.unshift(100)
+// console.log(arrey)
+
+let arrey = ["a", "b", "c", "d", "e"]
+
+arrey.forEach((value) => {
+    console.log(value)
+})
